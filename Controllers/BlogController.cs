4b1963760762a7cbbdcdf772814b1cd0311f93fb
Keenan Irusan <keenanirusan@gmail.com>
2020-08-06 10:06:05 +0200

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPostApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        public BlogController(AppDb db)
+        {
+            Db = db;
+        }
+
+        // GET api/blog
+        [HttpGet("AllBlogs")]
+        public async Task<IActionResult> GetLatest()
+        {
+            await Db.Connection.OpenAsync();
+            var query = new BlogPostQuery(Db);
+            var result = await query.LatestPostsAsync();
+            return new OkObjectResult(result);
+        }
+
+        // GET api/blog/5
+        [HttpGet("GetBlogById/{id}")]
+        public async Task<IActionResult> GetOne(int id)
+        {
+            await Db.Connection.OpenAsync();
+            var query = new BlogPostQuery(Db);
+            var result = await query.FindOneAsync(id);
+            if (result is null)
+                return new NotFoundResult();
+            return new OkObjectResult(result);
+        }
+
+        // POST api/blog
+        [HttpPost("InsertBlog")]
+        public async Task<IActionResult> Post([FromBody]BlogPost body)
+        {
+            await Db.Connection.OpenAsync();
+            body.Db = Db;
+            await body.InsertAsync();
+            return new OkObjectResult(body);
+        }
+
+        // PUT api/blog/5
+        [HttpPut("UpdateBlog/{id}")]
+        public async Task<IActionResult> PutOne(int id, [FromBody]BlogPost body)
+        {
+            await Db.Connection.OpenAsync();
+            var query = new BlogPostQuery(Db);
+            var result = await query.FindOneAsync(id);
+            if (result is null)
+                return new NotFoundResult();
+            result.Title = body.Title;
+            result.Content = body.Content;
+            await result.UpdateAsync();
+            return new OkObjectResult(result);
+        }
+
+        // DELETE api/blog/5
+        [HttpDelete("DeleteBlog/{id}")]
+        public async Task<IActionResult> DeleteOne(int id)
+        {
+            await Db.Connection.OpenAsync();
+            var query = new BlogPostQuery(Db);
+            var result = await query.FindOneAsync(id);
+            if (result is null)
+                return new NotFoundResult();
+            await result.DeleteAsync();
+            return new OkResult();
+        }
+
+        // DELETE api/blog
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAll()
+        {
+            await Db.Connection.OpenAsync();
+            var query = new BlogPostQuery(Db);
+            await query.DeleteAllAsync();
+            return new OkResult();
+        }
+
+        public AppDb Db { get; }
+    }
+}
